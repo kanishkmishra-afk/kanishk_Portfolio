b3557640f65c501105faaec0fd018db1fda86649
kanishkmishra-afk <kanishkm794@gmail.com>
2025-05-22 14:39:56 +0530

    upadate

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -36,7 +36,7 @@ export const MobileMenu=({menuOpen,setMenuOpen})=>{
                             About
                         </a>
 
-                        <a href="#project" 
+                        <a href="#projects" 
                         onClick={()=>setMenuOpen(false)}
                         className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300 
                         ${
